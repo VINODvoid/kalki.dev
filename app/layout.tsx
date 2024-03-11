026d172b053e36260308f0af980d206afc11a594
VINODvoid <iamlearner.v@gmail.com>
2024-03-12 00:28:31 +0530

    issue : contact error issue resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar";
+import TransitionProvider from "@/components/transitionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,15 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className=" w-screen h-screen bg-gradient-to-r from-rose-100 to-teal-100">
-          <div className="h-24">
-          <Navbar />
-          </div>
-          <div className="h-[calc(100vh-6rem)]">
-        {children}
-          </div>
-        </div>
-        </body>
+        <TransitionProvider>{children}</TransitionProvider>
+      </body>
     </html>
   );
+
 }
--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,13 @@ const Homepage = () => {
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         
         <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-          <Image src="/hero.png" alt="" fill className="object-contain" />
+          <Image src="/profile.png" alt="" fill className="object-contain" sizes="120" priority/>
         </div>
       
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
-            crafting Digital Experiences, Designing Tomorrow.
+            Crafting Digital Experiences, Designing Tomorrow.
           </h1>
         
           <p className="md:text-xl">
